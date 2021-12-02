8a8b219a0265d4b2f744d4ac0b49df415c7fbd80
songdaochuanshu <2484786273@qq.com>
2021-12-03 01:47:11 +0800

    更新 ‘服务体系’

---
+++ src/components/system/card.vue
@@ -0,0 +1,83 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: 松岛川树
+ * @Date: 2021-12-03 01:04:28
+ * @LastEditors: 松岛川树
+ * @LastEditTime: 2021-12-03 01:33:25
+ * @FilePath: \yifangda\src\components\system\card.vue
+-->
+<template>
+  <div class="container">
+    <div class="card">
+      <div class="card-header">
+        <h3>{{ introduction }}</h3>
+      </div>
+      <div class="card-body">
+        <div class="imgItem" v-for="item in imageList" :key="item">
+          <img width="356" height="200" :src="item" alt="" />
+        </div>
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    introduction: {
+      type: String,
+      default: "这是一段简介",
+    },
+    imageList: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="less">
+// pc端
+@media screen and (min-width: 1200px) {
+  .container {
+    width: 1200px;
+    margin: 0 auto;
+  }
+}
+
+// 移动端
+@media screen and (max-width: 1200px) {
+  .container {
+    width: 90%;
+    margin: 0 auto;
+  }
+}
+
+.card {
+  .card-header {
+    padding-top: 30px;
+    margin: 0 auto;
+    max-width: 1100px;
+    text-indent: 3em;
+  }
+  .card-body {
+    width: 100%;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 356px);
+    grid-gap: 20px;
+    justify-content: center;
+    padding: 40px;
+  }
+}
+</style>
--- src/views/System/convenience.vue
+++ src/views/System/convenience.vue
@@ -1,13 +1,38 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: 松岛川树
+ * @Date: 2021-12-02 19:57:28
+ * @LastEditors: 松岛川树
+ * @LastEditTime: 2021-12-03 01:37:19
+ * @FilePath: \yifangda\src\views\System\convenience.vue
+-->
 <template>
-  <div class="convenience">
-      <h1>便民服务</h1>
+  <div class="container">
+    <card :introduction="introduction" :imageList="imageList" />
   </div>
 </template>
 
 <script>
+import card from "@/components/system/card.vue";
 export default {
-  name: 'convenience',
+  name: "convenience",
   components: {
-  }
-}
+    card,
+  },
+  data() {
+    return {
+      introduction:
+        "便民服务属于公司衍生拓展业务，主要面对客户生活需求提供便利服务，目前，开设的便民业务有清洁服务（清洗油烟机、热水器、窗帘、擦拭玻璃、钟点服务等）、设施维修服务（上水故障处理、马桶及下水疏通、洁具更换，散热器清洗、维修及更换，电路维修及处理、锁具修理及更换等），约车代办服务、社区旅游、社区金融等业务。",
+      imageList: [
+        require("@/assets/img/System/convenience/蒙版组 17.png"),
+        require("@/assets/img/System/convenience/蒙版组 18.png"),
+        require("@/assets/img/System/convenience/蒙版组 19.png"),
+        require("@/assets/img/System/convenience/蒙版组 82.png"),
+        require("@/assets/img/System/convenience/蒙版组 83.png"),
+        require("@/assets/img/System/convenience/蒙版组 84.png"),
+      ],
+    };
+  },
+};
 </script>
