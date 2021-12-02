280ef18648a1740128d0f43596654ef54ebff5de
aboutnb <aboutnanbo@163.com>
2021-12-03 01:45:41 +0800

    新增“员工风采”

--- src/views/About/staffStyle.vue
+++ src/views/About/staffStyle.vue
@@ -1,11 +1,34 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: Xiaobo
+ * @Date: 2021-12-02 23:08:10
+ * @LastEditors: Xiaobo
+ * @LastEditTime: 2021-12-03 01:43:18
+ * @FilePath: \yifangda\src\views\About\staffStyle.vue
+-->
 <template>
   <div class="staffStyle">
-      <h1>员工风采</h1>
+    <div class="main">
+      <img src="../../assets/img/About/staffStyle/团队风采@2x.png" alt="" />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'staffStyle',
-}
+  name: "staffStyle",
+};
 </script>
+<style scoped lang='less'>
+.staffStyle {
+  width: 100%;
+  .main {
+    width: 80%;
+    margin: auto;
+    img {
+      width: 100%;
+    }
+  }
+}
+</style>
